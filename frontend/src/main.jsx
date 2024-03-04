80a7ffbe4bf06ddd0937a0d9d5c2bb2210e0ef81
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-03-04 00:56:57 -0800

    Add ErrorBoundary in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/router";
+import ErrorPage from "./pages/ErrorPage";
+import { ErrorBoundary } from "react-error-boundary";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Router />
-		</BrowserRouter>
+		<ErrorBoundary fallback={<ErrorPage />}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</ErrorBoundary>
 	</React.StrictMode>
 );
---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+	return (
+		<div className="text-center my-10">
+			<h2 className="text-xl text-red-500">Something went wrong</h2>
+		</div>
+	);
+};
+
+export default ErrorPage;
