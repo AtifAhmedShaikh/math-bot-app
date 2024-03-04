9570b787d83de354df8d4f9bb459c552da319377
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-03-04 00:25:41 -0800

    format files with prettier in backend

--- backend/src/utils/ApiError.js
+++ backend/src/utils/ApiError.js
@@ -8,7 +8,7 @@ class ApiError extends Error {
    * @param {number} statusCode - The HTTP status code.
    * @param {string} message - The error message.
    */
-  constructor (statusCode, message) {
+  constructor(statusCode, message) {
     super(message);
     this.statusCode = statusCode;
     this.message = message;
